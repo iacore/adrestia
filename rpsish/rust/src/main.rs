58312088ace05141305bd2a7b80ac980ef4d2de6
Jim Zhang <hyriodula@gmail.com>
2018-03-16 21:01:52 +0000

    traits

--- rpsish/rust/src/main.rs
+++ rpsish/rust/src/main.rs
@@ -5,66 +5,75 @@ use num::integer::Integer;
 use rand::Rng;
 use std::cmp::Ordering;
 
-type PayoffTable = Vec<Vec<i32>>;
+trait GameState<'a> {
+  type Action;
+  fn done(&self) -> bool;
+  fn apply_action(&mut self, &Self::Action);
+  fn moves(&self) -> Vec<Self::Action>;
+}
 
 #[derive(Debug)]
-struct GameConfig {
+struct RpsishConfig {
   num_symbols: usize,
-  payoffs: PayoffTable,
+  payoffs: Vec<Vec<i32>>,
   num_rounds: usize,
 }
 
 #[derive(Debug)]
-struct GameState<'a> {
-  config: &'a GameConfig,
-  cur_player: usize,
+struct RpsishState<'a> {
+  config: &'a RpsishConfig,
+  player: usize,
   public_symbols: [Vec<usize>; 2],
   private_symbols: [Vec<usize>; 2],
 }
 
 #[derive(Debug)]
-struct GameAction {
+struct RpsishAction {
+  player: usize,
   public_symbol: usize,
   private_symbol: usize,
+}
+
+impl<'a> GameState<'a> for RpsishState<'a> {
+  type Action = RpsishAction;
+
+  fn done(&self) -> bool {
+    let num_rounds = self.config.num_rounds;
+    self.private_symbols.into_iter().all(|v| v.len() >= num_rounds)
+  }
+  fn apply_action(&mut self, action: &RpsishAction) {
+    self.public_symbols[self.player].push(action.public_symbol);
+    self.private_symbols[self.player].push(action.private_symbol);
+    self.player = (self.player + 1) % 2;
+  }
+  fn moves(&self) -> Vec<RpsishAction> {
+    // TODO: implement
+    vec![]
+  }
+}
+
+impl<'a> RpsishState<'a> {
+  fn calc_score(&self) -> i32 {
+    let mut p1 = self.private_symbols[0].clone();
+    p1.append(&mut self.public_symbols[0].clone());
+    let mut p2 = self.private_symbols[1].clone();
+    p2.append(&mut self.public_symbols[1].clone());
+    let mut score = 0;
+    for &s in &p1 {
+      for &t in &p2 {
+        score += self.config.payoffs[s][t];
+      }
+    }
+    score
+  }
 }
 
 fn modulo<T: Integer + Copy>(x: T, m: T) -> T {
   ((x % m) + m) % m
 }
 
-fn apply_action(state: GameState, action: GameAction) -> GameState {
-  // TODO: Use a non-zero-cost-abstraction to make this more concise
-  let mut public_symbols = state.public_symbols.clone();
-  public_symbols[state.cur_player].push(action.public_symbol);
-  let mut private_symbols = state.private_symbols.clone();
-  private_symbols[state.cur_player].push(action.private_symbol);
-  GameState {
-    cur_player: (state.cur_player + 1) % 2,
-    public_symbols,
-    private_symbols,
-    ..state
-  }
-}
-
-fn game_done(state: &GameState) -> bool {
-  let num_rounds = state.config.num_rounds;
-  state.private_symbols.into_iter().all(|v| v.len() >= num_rounds)
-}
-
-fn calc_score(state: &GameState) -> i32 {
-  let mut p1 = state.private_symbols[0].clone(); p1.append(&mut state.public_symbols[0].clone());
-  let mut p2 = state.private_symbols[1].clone(); p2.append(&mut state.public_symbols[1].clone());
-  let mut score = 0;
-  for s in &p1 {
-    for t in &p2 {
-      score += state.config.payoffs[*s][*t];
-    }
-  }
-  score
-}
-
 fn main() {
-  let config = GameConfig {
+  let config = RpsishConfig {
     num_symbols: 3,
     /* 0 beats 1 beats 2 beats 0 */
     payoffs: vec![
@@ -74,21 +83,23 @@ fn main() {
     ],
     num_rounds: 5,
   };
-  let mut state = GameState {
+  let mut state = RpsishState {
     config: &config,
-    cur_player: 0,
+    player: 0,
     public_symbols:  [Vec::new(), Vec::new()],
     private_symbols: [Vec::new(), Vec::new()],
   };
-  while !game_done(&state) {
-    state = apply_action(state, GameAction {
+  while !state.done() {
+    let action = RpsishAction {
+      player:         state.player,
       public_symbol:  modulo(rand::thread_rng().gen::<usize>(), config.num_symbols),
       private_symbol: modulo(rand::thread_rng().gen::<usize>(), config.num_symbols),
-    });
+    };
+    state.apply_action(&action);
     println!("{:?}", state);
   }
 
-  let score = calc_score(&state);
+  let score = state.calc_score();
   println!("Final score: {:?}", score);
 
   match score.cmp(&0) {
