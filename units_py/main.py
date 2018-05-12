f12fc1c10f74d170cd1d4c537568c9066febde61
Charles Zinn <charles.zinn@gmail.com>
2018-05-12 14:24:35 -0400

    Add all units to Python version. Fort does not yet work.

--- units_py/main.py
+++ units_py/main.py
@@ -77,8 +77,27 @@ def add_resources_effect(resources: Resources) -> Callable[['Player'], None]:
 unit_kinds: Dict[str, UnitKind] = {
     'general': UnitKind(name='General', colour=Colour.BLACK, health=5, width=1, attack=[1], cost=None),
     'grunt': UnitKind(name='Grunt', colour=Colour.RED, health=1, width=1, attack=[1], cost=Resources(red=2)),
-    'font_b': UnitKind(name='Font R', colour=Colour.RED, health=2, width=1, attack=[], cost=Resources(red=1, blue=2),
+    'troll': UnitKind(name='Troll', colour=Colour.RED, health=2, width=2, attack=[4], cost=Resources(red=5)),
+    'wall': UnitKind(name='Wall', colour=Colour.GREEN, health=1, width=1, attack=[], cost=Resources(green=1)),
+    'turret': UnitKind(name='Turret', colour=Colour.GREEN, health=1, width=2, attack=[1], cost=Resources(green=2)),
+    'wide_wall': UnitKind(name='Wide Wall', colour=Colour.GREEN, health=2, width=4, attack=[], cost=Resources(green=3)),
+    'font_r': UnitKind(name='Font R', colour=Colour.BLUE, health=2, width=1, attack=[], cost=Resources(red=1, blue=2),
         before_turn=add_resources_effect(Resources(red=1))),
+    'font_g': UnitKind(name='Font G', colour=Colour.BLUE, health=2, width=1, attack=[], cost=Resources(green=1, blue=2),
+        before_turn=add_resources_effect(Resources(green=1))),
+    'font_b': UnitKind(name='Font B', colour=Colour.BLUE, health=2, width=1, attack=[], cost=Resources(blue=3),
+        before_turn=add_resources_effect(Resources(blue=1))),
+    'paragon': UnitKind(name='Paragon', colour=Colour.BLUE, health=2, width=1, attack=[], cost=Resources(red=1, green=1, blue=7),
+        before_turn=add_resources_effect(Resources(red=1, green=1, blue=1))),
+    'ogre': UnitKind(name='Ogre', colour=Colour.RED, health=2, width=2, attack=[2], cost=Resources(red=2, green=2)),
+    'shrek': UnitKind(name='Shrek', colour=Colour.GREEN, health=4, width=3, attack=[2], cost=Resources(red=2, green=3)),
+    'soldier': UnitKind(name='Soldier', colour=Colour.BLUE, health=1, width=1, attack=[1], cost=Resources(red=1, blue=2)),
+    'commando': UnitKind(name='Commando', colour=Colour.RED, health=2, width=1, attack=[1, 1], cost=Resources(red=3, blue=2)),
+    # TODO: forts don't take damage from red units
+    'fort': UnitKind(name='Fort', colour=Colour.GREEN, health=1, width=4, attack=[], cost=Resources(green=3, blue=2)),
+    'insurgent': UnitKind(name='Insurgent', colour=Colour.RED, health=3, width=1, attack=[1], cost=Resources(red=2, green=1, blue=1)),
+    'avatar': UnitKind(name='Avatar', colour=Colour.BLUE, health=3, width=2, attack=[1, 3, 1], cost=Resources(red=5, green=1, blue=3)),
+    'tank': UnitKind(name='Tank', colour=Colour.BLUE, health=10, width=7, attack=[2], cost=Resources(red=1, green=5, blue=3)),
 }
 
 class Unit:
@@ -197,7 +216,7 @@ if __name__ == '__main__':
         remaining_players = [p for p in state.players if p.alive]
         if len(remaining_players) < 2:
             if len(remaining_players) == 1:
-                print('{} wins!'.format(remaining_players[0]))
+                print('{} wins!'.format(remaining_players[0].name))
             else:
                 print('It is a tie!')
             break
