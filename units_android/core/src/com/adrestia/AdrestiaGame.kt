8caa04535b728e6fc2d6c089b90444eed89d08c9
Jim Zhang <hyriodula@gmail.com>
2018-06-06 10:58:41 -0400

    update stuff, and some random garbage

--- units_android/core/src/com/adrestia/AdrestiaGame.kt
+++ units_android/core/src/com/adrestia/AdrestiaGame.kt
@@ -6,20 +6,24 @@ import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
+import com.badlogic.gdx.scenes.scene2d.ui.Skin
 
 class AdrestiaGame : ApplicationAdapter() {
+    private var skin: Skin? = null
     private var camera: OrthographicCamera? = null
     private var batch: SpriteBatch? = null
     private var renderer: ShapeRenderer? = null
 
     override fun create() {
+        skin = Skin(Gdx.files.internal("ui/uiskin.json"))
         batch = SpriteBatch()
         camera = OrthographicCamera()
         renderer = ShapeRenderer()
+        camera!!.setToOrtho(false, 1600f, 900f)
     }
 
     override fun render() {
-        Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
+        Gdx.gl.glClearColor(0.08f, 0.1f, 0.2f, 1f)
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
 
         val r = renderer
@@ -29,9 +33,12 @@ class AdrestiaGame : ApplicationAdapter() {
             r.circle(Gdx.graphics.width / 2f, Gdx.graphics.height / 2f, 100f)
             r.end()
         }
+        camera!!.update()
     }
 
     override fun dispose() {
         batch!!.dispose()
     }
+
+    // Other methods: resize, pause, resume, dispose
 }
