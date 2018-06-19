b7d3886651f07e2c2a8e8c1e3aad783da31fd7d1
Jim Zhang <hyriodula@gmail.com>
2018-06-19 12:53:22 -0400

    Colour units, and better packing

--- units_godot/lib/colour.gd
+++ units_godot/lib/colour.gd
@@ -27,3 +27,12 @@ static func of_json(json):
     "BLUE": return BLUE
     "WHITE": return WHITE
   return BLACK
+
+static func to_color(colour):
+  match colour:
+    BLACK: return Color(0.5, 0.5, 0.5)
+    RED  : return Color(1.0, 0.0, 0.0)
+    GREEN: return Color(0.0, 1.0, 0.0)
+    BLUE : return Color(0.0, 0.0, 1.0)
+    WHITE: return Color(0.9, 0.9, 0.9)
+  return Color(0.0, 0.0, 0.0)
--- units_godot/scenes/game.gd
+++ units_godot/scenes/game.gd
@@ -2,6 +2,7 @@ extends VBoxContainer
 
 var unit_buy_bar = preload('res://components/unit_buy_bar.tscn')
 var BuildUnit = preload('res://lib/actions/build_unit.gd')
+var Colour = preload('res://lib/colour.gd')
 
 onready var r_label = $Toolbar/R
 onready var g_label = $Toolbar/G
@@ -13,6 +14,16 @@ onready var g = get_node("/root/global")
 onready var units = get_node("/root/UnitKinds").units
 var unit_bars = {}
 
+static func unit_fits(occupied, blocks, x, y):
+  for xy in blocks:
+    if occupied.has([xy[0]+x, xy[1]+y]):
+      return false
+  return true
+
+class SortUnitsByWidth:
+  static func sort(a, b):
+    return (a.kind.width > b.kind.width)
+
 func update_ui():
   var view = g.man.get_view()
 
@@ -23,19 +34,51 @@ func update_ui():
     unit_bars[unit].buy_button.disabled = !view.players[0].resources.subsumes(units[unit].cost)
   
   # Unit packing
-  # Right now is very bad; just makes each unit very tall and puts them in a single row
   for pid in range(view.players.size()):
     var player = view.players[pid]
+
+    var occupied = {}
     var packing_data = []
-    var x = 0
-    for unit_id in player.units:
+    var goal_aspect = 2.0 # Goal aspect ratio. >1 is wider.
+    var units = [] + player.units.values()
+    units.sort_custom(SortUnitsByWidth, "sort")
+    for unit in units:
+      var flat_blocks = unit.kind.tiles
       var blocks = []
-      var unit = player.units[unit_id]
-      for i in range(unit.kind.width):
-        blocks.append(0)
-        blocks.append(i)
-      # TODO jim: Different color for each unit.
-      packing_data.append([Color(0, 1, 0), [x, 0], blocks])
+      for i in range(flat_blocks.size() / 2):
+        blocks.append([flat_blocks[2 * i], flat_blocks[2 * i + 1]])
+
+      # Find a place to put this unit.
+      var result_x; var result_y
+      var x = 0
+      var y = 0
+      var done = false
+      while not done:
+        if x == 0 or x <= y * goal_aspect:
+          # Add a column
+          for i in range(0, y):
+            if unit_fits(occupied, blocks, x, i):
+              done = true
+              result_x = x
+              result_y = i
+              break
+          if not done:
+            x += 1
+        else:
+          # Add a row
+          for i in range(0, x):
+            if unit_fits(occupied, blocks, i, y):
+              done = true
+              result_x = i
+              result_y = y
+              break
+          if not done:
+            y += 1
+
+      for xy in blocks:
+        occupied[[xy[0] + result_x, xy[1] + result_y]] = true
+
+      packing_data.append([Colour.to_color(unit.kind.colour), [result_x, result_y], flat_blocks])
       x += 1
     Armies.get_child(1 - pid).data = packing_data
 
