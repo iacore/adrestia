aad5c1ecf6ce0644e3242158b5819c8f6c2576f7
neynt <hyriodula@gmail.com>
2018-07-19 14:13:06 -0400

    move godot C++ interface code to units_cpp/gdnative

--- units_godot/scenes/title.gd
+++ units_godot/scenes/title.gd
@@ -5,6 +5,7 @@ onready var g = get_node("/root/global")
 func _ready():
   get_tree().set_auto_accept_quit(true)
   get_tree().set_quit_on_go_back(true)
+  print("OS is %s" % [OS.get_name()])
 
 func _on_button_play_pressed():
   g.new_ai_game()
