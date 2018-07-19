d4e909c9dd691b2a14509ed7b8e76755f3dc2186
neynt <hyriodula@gmail.com>
2018-07-19 14:09:32 -0400

    Better indentation in colour.cc

--- units_cpp/colour.cc
+++ units_cpp/colour.cc
@@ -2,29 +2,16 @@
 
 void to_json(json& j, const Colour& colour) {
   switch (colour) {
-    case BLACK:
-      j = "BLACK";
-      break;
-    case RED:
-      j = "RED";
-      break;
-    case GREEN:
-      j = "GREEN";
-      break;
-    case BLUE:
-      j = "BLUE";
-      break;
+    case BLACK: j = "BLACK"; break;
+    case RED  : j = "RED"  ; break;
+    case GREEN: j = "GREEN"; break;
+    case BLUE : j = "BLUE" ; break;
   }
 }
 
 void from_json(const json& j, Colour& colour) {
-  if (j == "BLACK") {
-    colour = BLACK;
-  } else if (j == "RED") {
-    colour = RED;
-  } else if (j == "GREEN") {
-    colour = GREEN;
-  } else if (j == "BLUE") {
-    colour = BLUE;
-  }
+       if (j == "BLACK") { colour = BLACK; }
+  else if (j == "RED"  ) { colour = RED  ; }
+  else if (j == "GREEN") { colour = GREEN; }
+  else if (j == "BLUE" ) { colour = BLUE ; }
 }
