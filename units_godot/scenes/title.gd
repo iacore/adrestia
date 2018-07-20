35e7c003ba48325812baee9c7a4f52afef8d4cad
Charles Zinn <charles.zinn@gmail.com>
2018-07-20 09:19:15 -0400

    Add fade in to title screen

--- units_godot/scenes/title.gd
+++ units_godot/scenes/title.gd
@@ -6,6 +6,7 @@ func _ready():
   get_tree().set_auto_accept_quit(true)
   get_tree().set_quit_on_go_back(true)
   print("OS is %s" % [OS.get_name()])
+  $ColorRect/AnimationPlayer.play("LoadingFade");
 
 func _on_button_play_pressed():
   g.new_ai_game()
